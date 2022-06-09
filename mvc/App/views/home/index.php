d0453ef9d67699321e0417d365311c0f2bfa9b13
ilancabrita <82396649+ilancabrita@users.noreply.github.com>
2022-06-09 10:38:53 +0200

    maj cours 22.06.07

--- mvc/App/views/home/index.php
+++ mvc/App/views/home/index.php
@@ -5,7 +5,9 @@
       <form method="POST" action="/home/buscar">
         <div class="input-field">
           <input id="search" name="search" type="search" required>
-          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <label class="label-icon" for="search">
+              <i class="material-icons">search</i>
+            </label>
           <i class="material-icons">close</i>
         </div>
       </form>
